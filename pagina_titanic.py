62c1e736cf0a202455b2238bd0c8f94b9f3a9cbb
ivanmorales22 <157132658+ivanmorales22@users.noreply.github.com>
2024-04-18 22:42:37 -0600

    data correction

--- pagina_titanic.py
+++ pagina_titanic.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-path='data/test.csv'
+path='data/train.csv'
 df=pd.read_csv(path)
 
 males=df[(df['Sex'] == 'male')]
